9063b3d79c63bc9b0e6fd636b85104d5617ff736
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2021-04-11 19:44:54 +0200

    Lib update

--- Android.mk
+++ Android.mk
@@ -27,16 +27,16 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 # Build the beatsaber-hook shared library, SPECIFICALLY VERSIONED!
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_2_3
+LOCAL_MODULE := beatsaber-hook_1_2_6
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_2_3.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_0.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: codegen - version: 0.5.3
 include $(CLEAR_VARS)
 LOCAL_MODULE := codegen_0_7_0
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_7_0.so
+LOCAL_SRC_FILES := extern/libcodegen_0_7_1.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: questui - version: 0.3.1
 include $(CLEAR_VARS)
@@ -57,7 +57,7 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_2_3
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_2_6
 LOCAL_SHARED_LIBRARIES += codegen_0_7_0
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_SHARED_LIBRARIES += custom-types
